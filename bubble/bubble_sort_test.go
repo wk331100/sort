285f0505ae7b8191e963181d64e348670815e2ae
kunkkawu <kunkkawu@tencent.com>
2023-03-16 18:22:28 +0800

    添加冒泡排序

---
+++ bubble/bubble_sort_test.go
@@ -0,0 +1,41 @@
+package bubble
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+	"time"
+)
+
+var num = 10
+
+func TestBubbleSort(t *testing.T) {
+	data := testData()
+	sorted := make([][]int, 0)
+	for _, item := range data {
+		var a = make([]int, 0)
+		a = append(a, item...)
+		sorted = append(sorted, Sort(a))
+	}
+
+	for i := 0; i < len(data); i++ {
+		fmt.Println(data[i])
+		fmt.Println(sorted[i])
+		fmt.Println()
+	}
+
+}
+
+func testData() [][]int {
+	data := make([][]int, 0)
+	rand.Seed(time.Now().Unix())
+	for i := 0; i < num; i++ {
+		item := make([]int, 0)
+		for j := 0; j < 10; j++ {
+			item = append(item, rand.Intn(100))
+		}
+		data = append(data, item)
+	}
+
+	return data
+}
